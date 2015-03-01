bcf3f7c6e2fbed0d6434e43281d90befaf9c4add
Darren Ehlers <me@darrenehlers.com>
2015-03-01 15:17:50 -0600

    Initial blank project
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

---
+++ GoAlive.ios/GoAlive.ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoAlive.ios
+//
+//  Created by Darren Ehlers on 3/1/15.
+//  Copyright (c) 2015 Darren Ehlers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoAlive.ios/GoAlive.ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoAlive.ios
+//
+//  Created by Darren Ehlers on 3/1/15.
+//  Copyright (c) 2015 Darren Ehlers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
